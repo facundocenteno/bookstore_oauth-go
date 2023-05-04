5c3eb242d573d83099eff9afb1cf551f793ae3ab
facundocenteno <facucenteno1994@gmail.com>
2023-05-04 08:03:40 -0300

    changes in oauth.go

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -75,6 +75,9 @@ func AuthenticateRequest(request *http.Request) *errors.RestErr {
 	}
 	at, err := getAccessToken(accessTokenId)
 	if err != nil {
+		if err.Status == http.StatusNotFound {
+			return nil
+		}
 		return err
 	}
 	request.Header.Add(headerXClientId, fmt.Sprintf("%v", at.ClientId))
@@ -102,6 +105,7 @@ func getAccessToken(accessTokenId string) (*accessToken, *errors.RestErr) {
 		if err := json.Unmarshal(response.Bytes(), &restErr); err != nil {
 			return nil, errors.NewInternalServerError("invalid error interface when trying to get access token")
 		}
+
 		return nil, &restErr
 	}
 	var at accessToken
